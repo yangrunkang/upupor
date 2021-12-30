318673ea2f33773ad0cfcdc6be9b7331b845bce4
yangrunkang <yangrunkang53@gmail.com>
2021-12-31 00:17:13 +0800

    Add: 文件添加MIT开源许可

--- upupor-web/src/main/resources/db/migration/V20201002013429__init_tag.sql
+++ upupor-web/src/main/resources/db/migration/V20201002013429__init_tag.sql
@@ -1,3 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 yangrunkang
+ *
+ * Author: yangrunkang
+ * Email: yangrunkang53@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /*BEGIN;
 INSERT INTO `tag` VALUES (1, 'back-end', 1, '后端', 0, 'https://upupor-img.oss-cn-hangzhou.aliyuncs.com/icons/background.png', 1582824705, '2020-04-15 11:35:19');
 INSERT INTO `tag` VALUES (2, 'front-end', 1, '前端', 0, 'https://upupor-img.oss-cn-hangzhou.aliyuncs.com/icons/web.png', 1582824706, '2020-04-15 11:35:58');
--- upupor-web/src/main/resources/db/migration/V20201015010756__content_add_column_initial_release_status.sql
+++ upupor-web/src/main/resources/db/migration/V20201015010756__content_add_column_initial_release_status.sql
@@ -1,2 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 yangrunkang
+ *
+ * Author: yangrunkang
+ * Email: yangrunkang53@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 alter table content add column `is_initial_status` int(2) DEFAULT 0 COMMENT '是否是第一次发布 0-未发布 1-已发布';
 update content set is_initial_status = 1 where status = 0;
