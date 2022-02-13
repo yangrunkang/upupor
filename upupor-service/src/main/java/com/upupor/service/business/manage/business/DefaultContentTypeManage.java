0c606415921f0706e477ca456a9216902e052122
yangrunkang <yangrunkang53@gmail.com>
2022-02-13 11:22:44 +0800

    Mod: code review

--- upupor-service/src/main/java/com/upupor/service/business/manage/business/DefaultContentTypeManage.java
+++ upupor-service/src/main/java/com/upupor/service/business/manage/business/DefaultContentTypeManage.java
@@ -28,16 +28,9 @@
 package com.upupor.service.business.manage.business;
 
 import com.upupor.framework.CcConstant;
-import com.upupor.service.business.aggregation.dao.entity.Member;
-import com.upupor.service.business.aggregation.service.FileService;
-import com.upupor.service.business.aggregation.service.MemberService;
 import com.upupor.service.business.manage.AbstractManageInfoGet;
 import com.upupor.service.business.manage.ManageDto;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author cruise
@@ -46,24 +39,8 @@ import java.util.List;
 @Component
 public class DefaultContentTypeManage extends AbstractManageInfoGet {
 
-    @Resource
-    private MemberService memberService;
-
-    @Resource
-    private FileService fileService;
-
     @Override
     protected void specifyDtoHandle(ManageDto manageDto) {
-        String userId = manageDto.getUserId();
-
-        Member member = memberService.memberInfoData(userId);
-        // 获取用户历史头像
-        List<String> userHistoryViaList = fileService.getUserHistoryViaList(member.getUserId());
-        if (!CollectionUtils.isEmpty(userHistoryViaList)) {
-            member.setHistoryViaList(userHistoryViaList);
-        }
-        getMemberIndexDto().setMember(member);
-
     }
 
 
