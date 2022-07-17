d17a7e93d048c2c116cea4855716a7f2322bdae8
yangrunkang <yangrunkang53@gmail.com>
2022-07-17 21:39:05 +0800

    Mod: 生成环境图片资源移除域名,使用/开头来加载资源,便于后续迁移只需要改nginx配置即可

--- upupor-framework/src/main/java/com/upupor/framework/config/UpuporConfig.java
+++ upupor-framework/src/main/java/com/upupor/framework/config/UpuporConfig.java
@@ -1,28 +1,30 @@
 /*
- * MIT License
- *
- * Copyright (c) 2021-2022 yangrunkang
- *
- * Author: yangrunkang
- * Email: yangrunkang53@gmail.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * <!--
+ *   ~ MIT License
+ *   ~
+ *   ~ Copyright (c) 2021-2022 yangrunkang
+ *   ~
+ *   ~ Author: yangrunkang
+ *   ~ Email: yangrunkang53@gmail.com
+ *   ~
+ *   ~ Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   ~ of this software and associated documentation files (the "Software"), to deal
+ *   ~ in the Software without restriction, including without limitation the rights
+ *   ~ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   ~ copies of the Software, and to permit persons to whom the Software is
+ *   ~ furnished to do so, subject to the following conditions:
+ *   ~
+ *   ~ The above copyright notice and this permission notice shall be included in all
+ *   ~ copies or substantial portions of the Software.
+ *   ~
+ *   ~ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   ~ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   ~ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   ~ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   ~ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   ~ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   ~ SOFTWARE.
+ *   -->
  */
 
 package com.upupor.framework.config;
@@ -83,7 +85,7 @@ public class UpuporConfig {
      * @return
      */
     public String getOssStaticPrefix() {
-        String requestUrl = getMinio().getRequestUrl();
+        String requestUrl = getRequestUrl();
         String nginxRouter = getMinio().getNginxRouter();
 
         if (ossSource.equals(OssSource.ALI_OSS)) {
@@ -100,8 +102,21 @@ public class UpuporConfig {
         throw new BusinessException(ErrorCode.UNKNOWN_EXCEPTION, "系统未适配来源");
     }
 
+    /**
+     * 获取请求Url
+     *
+     * @return
+     */
+    private String getRequestUrl() {
+        if ("prd".equals(env)) {
+            return StringUtils.EMPTY;
+        } else {
+            return getMinio().getRequestUrl();
+        }
+    }
+
     public String getUploadFilePrefix() {
-        String requestUrl = getMinio().getRequestUrl();
+        String requestUrl = getRequestUrl();
         String nginxRouter = getMinio().getNginxRouter();
 
         if (ossSource.equals(OssSource.ALI_OSS)) {
@@ -110,7 +125,7 @@ public class UpuporConfig {
 
         if (ossSource.equals(OssSource.MINIO_OSS)) {
             // 非生产
-            if(!"prd".equals(env)){
+            if (!"prd".equals(env)) {
                 return getMinio().getEndpoint() + CcConstant.BACKSLASH + getMinio().getBucketName() + CcConstant.BACKSLASH;
             }
 
